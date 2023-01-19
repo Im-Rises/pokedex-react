f0462a2fe1d7eb0e5f70c3dd6f2cf5095b275404
clementreiffers <reiffersclement@gmail.com>
2023-01-19 13:02:43 +0100

    recherche ok

--- src/App/Components/List/ListPkm.jsx
+++ src/App/Components/List/ListPkm.jsx
@@ -1,32 +1,43 @@
 import React, {useEffect, useState} from 'react';
-import {makeRequest} from '../../Requests';
-import {PkmMemberList} from './PkmMemberList';
+import {getAllInfoOfPokemon, makeRequest} from '../../Requests';
 import PropTypes from 'prop-types';
+import {PkmMemberList} from './PkmMemberList';
 
-export const ListPkm = ({nbrOfPkm, handlePokemonToShow}) => {
+export const ListPkm = ({nbrOfPkm, handlePokemonToShow, pokemon}) => {
 	const [state, setState] = useState({pkmList: []});
 
 	const handlePkmList = pkmList => setState({...state, pkmList});
 
+	const emptyPkmList = () => setState({...state, pkmList: []});
+
 	useEffect(() => {
-		makeRequest(`https://pokeapi.co/api/v2/pokemon?limit=${nbrOfPkm}`)
-			.then((({results}) => results))
-			.then(res => res.map(v => v.name))
-			.then(handlePkmList);
-	}, [nbrOfPkm]);
+		if (!pokemon || pokemon === '') {
+			makeRequest(`https://pokeapi.co/api/v2/pokemon?limit=${nbrOfPkm}`)
+				.then(({results}) => results)
+				.then(res => res.map(({name, url}) => [name, url.split('/')[6]]))
+				.then(handlePkmList)
+				.catch(emptyPkmList);
+		} else {
+			console.log('here');
+			getAllInfoOfPokemon(pokemon)
+				.then(({name, id}) => [[name, id]])
+				.then(handlePkmList);
+		}
+	}, [nbrOfPkm, pokemon]);
 
 	return <form>
 		{
-			state.pkmList.map((pkm, i) =>
-				<label key={i + 1}>
-					<input type={'radio'} onClick={handlePokemonToShow} name={'pkm'} style={{display: 'none'}} value={pkm}/>
-					<PkmMemberList pkm={pkm} pkmIndex={i + 1}/>
-				</label>,
-			)}
+			state.pkmList.map(([name, id]) =>
+				<label key={id}>
+					<input type={'radio'} onClick={handlePokemonToShow} name={'pkm'} style={{display: 'none'}}
+						value={name}/>
+					<PkmMemberList pkm={name} pkmIndex={id}/>
+				</label>)}
 	</form>;
 };
 
 ListPkm.propTypes = {
-	nbrOfPkm: PropTypes.number,
+	nbrOfPkm: PropTypes.number.isRequired,
+	pokemon: PropTypes.string,
 	handlePokemonToShow: PropTypes.func.isRequired,
 };
--- src/App/Page/List.jsx
+++ src/App/Page/List.jsx
@@ -10,6 +10,8 @@ export const List = () => {
 
 	const handleImageToShow = imageToShow => setState({...state, imageToShow});
 
+	const handleSearchPkm = ({target: {value}}) => setState({...state, search: value});
+
 	useEffect(() => {
 		getSprites(state.pokemonToShow)
 			.then(({other}) => other['official-artwork'].front_default)
@@ -19,14 +21,14 @@ export const List = () => {
 	return <div>
 		<div className={'name-searchbar'}>
 			<div className={'pokemon-name'}><p>{state.pokemonToShow}</p></div>
-			<div className={'pokemon-name'}><input type={'search'}/></div>
+			<div className={'pokemon-name'}><input type={'search'} onChange={handleSearchPkm} value={state.search}/></div>
 		</div>
 		<div className={'list-and-preview'}>
 			<div className={'left'}>
 				<img src={state.imageToShow} alt={''} style={{width: '100px'}}/>
 			</div>
 			<div className={'right'}>
-				<ListPkm nbrOfPkm={MAX_PKM} handlePokemonToShow={handlePokemonToShow}/>
+				<ListPkm nbrOfPkm={MAX_PKM} handlePokemonToShow={handlePokemonToShow} pokemon={state.search}/>
 			</div>
 		</div>
 	</div>;
