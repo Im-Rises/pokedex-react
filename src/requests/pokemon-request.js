8d6487a58c4a483631261135a22ce39837a1b950
clementreiffers <reiffersclement@gmail.com>
2023-05-29 13:05:39 +0200

    getPokemonFlavourEntryWithVersion refacto en lambda

--- src/requests/pokemon-request.js
+++ src/requests/pokemon-request.js
@@ -1,5 +1,6 @@
 import {LANGUAGE_NAME} from '../constants/pokedex-constant';
 import {jsonify} from './main-request';
+import {andThen, applySpec, filter, map, path, pipe, pipeWith, prop, replace} from 'ramda';
 
 const getArtwork = requestResult => requestResult?.sprites?.front_default;
 
@@ -7,18 +8,21 @@ const getIcon = requestResult => requestResult?.sprites?.versions['generation-vi
 
 const getPokemonTypes = requestResult => requestResult?.types?.map(t => t?.type?.name);
 
-const getPokemonFlavourEntryWithVersion = async requestResult =>
-	fetch(requestResult?.species?.url)
-		.then(jsonify)
-		.then(data => data.flavor_text_entries)
-		.then(flavorTextEntries => {
-			const filteredEntries = flavorTextEntries.filter(flavorTextEntry => flavorTextEntry.language.name === LANGUAGE_NAME);
-			return {
-				gameVersion: filteredEntries.map(flavorTextEntry => flavorTextEntry.version.name)
-					.map(gameVersion => gameVersion.replace(/[\n\f\r]/g, ' ')),
-				flavorText: filteredEntries.map(flavorTextEntry => flavorTextEntry.flavor_text)
-					.map(flavorText => flavorText.replace(/[\n\f\r]/g, ' ')),
-			};
-		});
+const getSpeciesUrl = requestResult => requestResult?.species?.url;
 
-export {getPokemonTypes, getPokemonFlavourEntryWithVersion, getIcon, getArtwork};
+const fetchSpecies = requestResult => fetch(getSpeciesUrl(requestResult));
+
+const isCorrectLanguage = flavourTextEntry => flavourTextEntry?.language?.name === LANGUAGE_NAME;
+
+const getPokemonFlavourEntryWithVersion = requestResult => pipeWith(andThen)([
+	fetchSpecies,
+	jsonify,
+	prop('flavor_text_entries'),
+	filter(isCorrectLanguage),
+	applySpec({
+		gameVersion: map(pipe(path(['version', 'name']), replace(/[\n\f\r]/g, ' '))),
+		flavorText: map(pipe(prop('flavor_text'), replace(/[\n\f\r]/g, ' '))),
+	}),
+])(requestResult);
+
+export {getPokemonTypes, getIcon, getArtwork, getPokemonFlavourEntryWithVersion};
