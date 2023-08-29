4957f8047cb36494e5421731c4e3d6bc2bddc172
Im-Rises <quentin-morel88@hotmail.com>
2023-08-28 23:26:39 -0400

    Added request for version

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,12 +1,16 @@
 import {always, andThen, applySpec, identity, ifElse, pipeWith, prop} from 'ramda';
 import {getArtwork, getIcon, getPokemonFlavourEntryWithVersion, getPokemonTypes} from './pokemon-request';
 import {getPokemonNumber} from './pokedex-request';
-import {API_URL} from '../constants/pokedex-constant.js';
+import {API_URL, LANGUAGE_NAME} from '../constants/pokedex-constant.js';
 
 const jsonify = data => data.json();
 
 const getPokemon = pokemonName =>
 	fetch(`${API_URL}/pokemon/${pokemonName}`)
+		.then(jsonify);
+
+const getVersion = version =>
+	fetch(`${API_URL}/version/${version}`)
 		.then(jsonify);
 
 const isUndefined = value => value === undefined;
@@ -31,6 +35,10 @@ const getAllFromPokemon = async pokemon =>
 			flavourEntries: prop('flavourEntries'),
 		}),
 	])(pokemon);
+
+const getVersionCompleteName = version => getVersion(version).then(prop('names')).then(names => names.find(name => name.language.name === LANGUAGE_NAME).name);
+
+getVersionCompleteName('red').then(console.log);
 
 const uppercaseFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
 
