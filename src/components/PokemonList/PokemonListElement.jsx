17dc6bda0ea06045a410c54a11328bfb37e3c475
clementreiffers <reiffersclement@gmail.com>
2023-09-07 00:06:23 +0200

    adding lazy loading images

--- src/components/PokemonList/PokemonListElement.jsx
+++ src/components/PokemonList/PokemonListElement.jsx
@@ -1,7 +1,31 @@
 import PropTypes from 'prop-types';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {getAllFromPokemon} from '../../requests/index.js';
 import React from 'react';
+
+const LazyLoadImage = props => {
+	const imageRef = useRef();
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					// Charger l'image lorsque l'élément devient visible
+					imageRef.current.src = props.src;
+					observer.unobserve(imageRef.current);
+				}
+			});
+		});
+
+		observer.observe(imageRef.current);
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [props.src]);
+
+	return <img ref={imageRef} alt='' />;
+};
 
 export const PokemonListElement = props => {
 	const [icon, setIcon] = useState('');
@@ -12,11 +36,14 @@ export const PokemonListElement = props => {
 	}, [props.pokemonName]);
 
 	return <div>
-		{icon && <img src={icon} alt={''}/>}
+		{icon && <LazyLoadImage src={icon} />}
 		{props.pokemonName}
 	</div>;
 };
 
 PokemonListElement.propTypes = {
 	pokemonName: PropTypes.string.isRequired,
+};
+LazyLoadImage.propTypes = {
+	src: PropTypes.string.isRequired,
 };
