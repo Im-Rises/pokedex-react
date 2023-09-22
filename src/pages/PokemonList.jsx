f7e06876223e3d9fa8275bad48d77601627d3711
Im-Rises <quentin-morel88@hotmail.com>
2023-09-21 20:51:04 -0400

    wip

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {getListOfPkmAvailable} from '../requests/pokedex-request.js';
 import {MAX_PKM} from '../constants/pokedex-constant.js';
-import pokeballLoadingImage from '../images/loading/pokeball_loading.gif';
+import pokeballLoadingImage from '../images/loading/pokeball-loading-150x150.gif';
 import './PokemonList.scss';
 import {pipe, pluck, prop} from 'ramda';
 import {getAllFromPokemon, uppercaseFirstLetter} from '../requests/index.js';
@@ -98,7 +98,7 @@ export const PokemonList = () => {
 				<div className={'pokemon-name'}>
 					{pokemon.select ? uppercaseFirstLetter(pokemon.select) : 'Select a pokemon!'}
 				</div>
-				<div>
+				<div className={'pokemon-artwork-holder'}>
 					{pokemon.select ? <img src={pokemon.officialArtwork} alt={'official artwork'}/> : <></>}
 				</div>
 				{pokemon.select
