aabf2fceb4c5d8e61a0cad21c4dc33673a2c895a
clementreiffers <reiffersclement@gmail.com>
2023-10-03 23:07:53 +0200

    capitalize first letter of pokemon using css

--- src/components/PokemonList/PokemonListElement.jsx
+++ src/components/PokemonList/PokemonListElement.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import {useEffect, useState} from 'react';
-import {getAllFromPokemon, uppercaseFirstLetter} from '../../requests/index.js';
+import {getAllFromPokemon} from '../../requests/index.js';
 import React from 'react';
 import LazyLoadImage from '../LazyLoadImage/LazyLoadImage.jsx';
+import './pokemon-list-element.scss';
 
 export const PokemonListElement = props => {
 	const [icon, setIcon] = useState('');
@@ -12,9 +13,9 @@ export const PokemonListElement = props => {
 			.then(({icon}) => setIcon(icon));
 	}, [props.pokemonName]);
 
-	return <div>
-		 {icon && <LazyLoadImage src={icon}/>}
-		{uppercaseFirstLetter(props.pokemonName)}
+	return <div className={'pokemon-list-element'}>
+		{icon && <LazyLoadImage src={icon}/>}
+		{props.pokemonName}
 	</div>;
 };
 
