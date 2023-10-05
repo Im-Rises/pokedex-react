6f88aae0a67cb306b287cc97e8c936bf2729461a
clementreiffers <reiffersclement@gmail.com>
2023-10-05 12:30:08 +0200

    adding description of the pokedex

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -4,13 +4,14 @@ import {MAX_PKM} from '../constants/pokedex-constant.js';
 import pokeballLoadingImage from '../images/loading/pokeball-loading-150x150.gif';
 import './PokemonList.scss';
 import {pipe, pluck, prop} from 'ramda';
-import {getAllFromPokemon} from '../requests/index.js';
+import {getAllFromPokemon, getPokemon} from '../requests/index.js';
 import {PokemonDetails} from './PokemonDetails.jsx';
 import {PokemonListComponent} from '../components/PokemonList/PokemonListComponent.jsx';
 import {easterEggPokemonData} from '../constants/pokemon-data-fetch.js';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import defaultIcon from '../images/loading/pokeball-loading-150x150.gif';
+import LazyLoadImage from '../components/LazyLoadImage/LazyLoadImage.jsx';
+import {getArtwork} from '../requests/pokemon-request.js';
 
 const getAllPokemonName = pipe(prop('results'), pluck('name'));
 
@@ -37,9 +38,7 @@ export const PokemonList = () => {
 	const handleOfficialArtwork = ({officialArtwork}) =>
 		setPokemon({...pokemon, officialArtwork});
 
-	const defaultPokemonSet = () => setPokemon({
-		...pokemon, listShows: pokemonList, select: pokemonList[0],
-	});
+	const defaultPokemonSet = () => setPokemon({...pokemon, listShows: pokemonList});
 
 	// get all pokemon list
 	useEffect(() => {
@@ -55,7 +54,7 @@ export const PokemonList = () => {
 			getAllFromPokemon(select)
 				.then(handleOfficialArtwork);
 		}
-	}, [pokemon.select, pokemonList]);
+	}, [pokemon.select]);
 
 	// manage search
 	useEffect(() => {
@@ -77,8 +76,6 @@ export const PokemonList = () => {
 			});
 			return;
 		}
-
-		// setEasterEggActivated(false);
 
 		const timer = setTimeout(() => {
 			const listShows = pokemonList
@@ -105,12 +102,25 @@ export const PokemonList = () => {
 						{pokemon.select ? pokemon.select : 'Select a pokemon!'}
 					</div>
 					<div className={'pokemon-artwork-holder'}>
-						<img src={pokemon.select ? pokemon.officialArtwork : defaultIcon} alt={'official artwork'}/>
+						{pokemon.select
+							? <LazyLoadImage imageGetter={() => getPokemon(pokemon.select).then(getArtwork)} />
+							: <div>
+								Welcome to the Pokédex, your ultimate Pokémon companion! Our user-friendly interface makes it easy to explore and learn about your favorite Pokémon.
+
+								Here's how it works:
+
+								<ol>
+									<li><b>Pokémon Gallery : </b> On the right, you'll find a list of all available Pokémon. Simply click on any Pokémon to view its full-size image.</li>
+									<li><b>Detailed Information: </b> Below the Pokémon image, you'll discover a button to access detailed information. Click it, and you'll uncover a wealth of knowledge about the selected Pokémon, including its abilities, type, and much more!</li>
+									<li><b>Search Feature : </b> Looking for a specific Pokémon? No problem! Use the search bar in the top-right corner to quickly find the Pokémon you're interested in.</li>
+									<li><b>Region-specific Details : </b> Dive deeper into the world of Pokémon by exploring region-specific information. Discover the habitat, behaviors, and unique characteristics of each Pokémon based on their region of origin.</li>
+								</ol>
+
+								The Pokédex is your gateway to the fascinating world of Pokémon. Explore, learn, and embark on your journey to become a Pokémon Master!</div>
+						}
 					</div>
 					<div className={'pokemon-view-details-button-holder'}>
-						{pokemon.officialArtwork !== pokeballLoadingImage
-                            && <button onClick={toggleViewDetails}>View
-                                details</button>}
+						{pokemon.select && <button onClick={toggleViewDetails}>View details</button>}
 					</div>
 				</div>
 				<div className={'right'}>
