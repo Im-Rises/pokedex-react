f025a13ba78ab9f9915c3d03c448ecebf0ec3e1f
Im-Rises <quentin-morel88@hotmail.com>
2023-11-09 22:05:20 -0500

    wip

--- src/components/PokemonDetailsDumb/PokemonDetailsDumb.jsx
+++ src/components/PokemonDetailsDumb/PokemonDetailsDumb.jsx
@@ -32,34 +32,36 @@ export const PokemonDetailsDumb = props => {
 							<img src={props.pokemonData.officialArtwork} alt={props.pokemonData.pokemonName}/>
 						</div>
 					</div>
-					<div className={'pokemon-infos'}>
-						<div className={'pokemon-description'}>
-							<h2>Description</h2>
-							<select name='description' id='description'
-								// onChange={event => setPokemonDescription(props.pokemonData.flavorEntries.flavorText[event.target.value])}>
-								onChange={event => setPokemonVersion(event.target.value)}>
-								{props.pokemonData.flavorEntries && props.pokemonData.flavorEntries.gameVersion.map(
-									(gameVersion, index) => <option key={gameVersion}
-										value={index}>{gameVersion}</option>,
-								)}
-							</select>
-							<p>{pokemonDescription}</p>
-						</div>
-						<div className={'pokemon-size-types'}>
-							<ul>
-								{R.map(
-									type => <li key={type}>
-										<p><img
-											src={pokemonTypeConstant[type]}
-											alt={type}
-										/>{type}</p>
-									</li>,
-									props.pokemonData.type,
-								)}
-							</ul>
-							<div>
-								<p>Height: {props.pokemonOtherInfo.height} </p>
-								<p>Weight: {props.pokemonOtherInfo.weight} </p>
+					<div className={'pokemon-infos-holder'}>
+						<div className={'pokemon-infos'}>
+							<div className={'pokemon-description'}>
+								<h2>Description</h2>
+								<select name='description' id='description'
+									// onChange={event => setPokemonDescription(props.pokemonData.flavorEntries.flavorText[event.target.value])}>
+									onChange={event => setPokemonVersion(event.target.value)}>
+									{props.pokemonData.flavorEntries && props.pokemonData.flavorEntries.gameVersion.map(
+										(gameVersion, index) => <option key={gameVersion}
+											value={index}>{gameVersion}</option>,
+									)}
+								</select>
+								<p>{pokemonDescription}</p>
+							</div>
+							<div className={'pokemon-size-types'}>
+								<ul>
+									{R.map(
+										type => <li key={type}>
+											<p><img
+												src={pokemonTypeConstant[type]}
+												alt={type}
+											/>{type}</p>
+										</li>,
+										props.pokemonData.type,
+									)}
+								</ul>
+								<div>
+									<p>Height: {props.pokemonOtherInfo.height} </p>
+									<p>Weight: {props.pokemonOtherInfo.weight} </p>
+								</div>
 							</div>
 						</div>
 					</div>
