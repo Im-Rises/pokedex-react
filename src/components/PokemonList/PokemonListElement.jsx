76d7aae3e53e2c87a61d0dd6b666c39aef899b60
Im-Rises <quentin-morel88@hotmail.com>
2023-11-15 22:21:39 -0500

    Corrected size of pokemon icon in pokemon list

--- src/components/PokemonList/PokemonListElement.jsx
+++ src/components/PokemonList/PokemonListElement.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import {getAllFromPokemon} from '../../requests/index.js';
 import React from 'react';
 import LazyLoadImage from '../LazyLoadImage/LazyLoadImage.jsx';
+import './PokemonListElement.scss';
 
 export const PokemonListElement = props => {
 	const imageGetter = () =>
@@ -9,7 +10,7 @@ export const PokemonListElement = props => {
 			.then(({icon}) => icon);
 
 	return <div className={'pokemon-list-element'}>
-		{<LazyLoadImage imageGetter={imageGetter} />}
+		{<LazyLoadImage imageGetter={imageGetter}/>}
 		{props.pokemonName}
 	</div>;
 };
